22e4ff088e0604f78b759546c616af780da41660
AbhiGone <giriabhinaya@gmail.com>
2023-09-15 09:22:53 -0400

    part 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int seasonOne;
+        int seasonTwo;
+        int seasonThree;
+        int seasonFour;
+
+        System.out.println("Input values for the maintenance costs for each of the four seasons.");
+        seasonOne = scan.nextInt();
+        seasonTwo = scan.nextInt();
+        seasonThree = scan.nextInt();
+        seasonFour = scan.nextInt();
+
+        int total = seasonOne + seasonTwo + seasonThree + seasonFour;
+        System.out.println("Your yearly cost for all the season combined is, " + total);
     }
 }
